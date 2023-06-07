c84b531bf4c3f5bfb46e64796620ddc999920d30
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2023-06-07 12:52:16 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 
+
+        $("#birthday").datepicker({
+            onSelect: function (value, ui) {
+                var today = new Date();
+                age = today.getFullYear() - ui.selectedYear;
+                if (age < 18) {
+                    $('.date_notice').html('Devi avere almeno 18 anni!');
+                    $(".reg_single_submit input").addClass('not_clickable');
+                } else {
+                    $('.date_notice').html('');
+                    $(".reg_single_submit input").removeClass('not_clickable');
+                }
+            },
+            
+            maxDate: new Date(),
+            changeYear: true,
+            changeMonth: true,
+            yearRange: "-100:-0",
+            maxDate: "0Y",
+            minDate: "-100Y",
+            monthNamesShort: ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"],
+        });
